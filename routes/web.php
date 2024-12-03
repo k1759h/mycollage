2d74f0db19c3d6a3379ed7031abaac6967394b31
k1759h <sonmond421@gmail.com>
2024-12-03 20:14:30 +0000

    12/4 update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\CollageControler;
+Route::prefix('admin')->group(function () {
+    Route::resource('works', CollageController::class)->except(['show']);
+});
